7e03e652da4af250cd67062f495c4a10dc13fd9b
kurema <kurema_makoto_software@yahoo.co.jp>
2023-01-10 13:21:18 +0900

    Wasm works!

--- AozoraEditor/AozoraLibraryWasm/Program.cs
+++ AozoraEditor/AozoraLibraryWasm/Program.cs
@@ -1,18 +1,60 @@
 using System;
+using System.Linq;
 using System.Runtime.InteropServices.JavaScript;
 
-Console.WriteLine("Hello, Browser!");
+//public partial class MyClass
+//{
+//    [JSExport]
+//    internal static string Greeting()
+//    {
+//        var text = $"Hello, World! Greetings from {GetHRef()}";
+//        Console.WriteLine(text);
+//        return text;
+//    }
 
-public partial class MyClass
+//    [JSImport("window.location.href", "main.js")]
+//    internal static partial string GetHRef();
+//}
+
+{ };
+
+public partial class AozoraFunctions
 {
     [JSExport]
-    internal static string Greeting()
+    internal static string Aozora2html(string text)
     {
-        var text = $"Hello, World! Greetings from {GetHRef()}";
-        Console.WriteLine(text);
-        return text;
+        var jstream = new Aozora.JstreamString(text, false);
+        var output = new Aozora.Helpers.OutputString();
+        //var output = new Aozora.Helpers.OutputConsole();
+        var aozora = new Aozora.Aozora2Html(jstream, output, new Aozora.Helpers.OutputConsoleError(), "./gaiji/", null)
+        {
+            UseJisx0213Accent = true,
+            UseJisx0214EmbedGaiji = true,
+            UseUnicodeEmbedGaiji = true,
+        };
+        aozora.Process();
+        return output?.ToString();
     }
 
-    [JSImport("window.location.href", "main.js")]
-    internal static partial string GetHRef();
+    [JSImport("WriteIframe", "main.js")]
+    internal static partial void WriteIframe(string s);
+
+    [JSImport("GetInputText", "main.js")]
+    internal static partial string GetInputText();
+
+    [JSExport]
+    internal static void Convert()
+    {
+        var input = GetInputText();
+        string output = string.Empty;
+        try
+        {
+            output = Aozora2html(input);
+        }
+        catch(Exception e) {
+            Console.WriteLine($"Error: {e.Message}");
+            Console.WriteLine(e.StackTrace);
+        }
+        WriteIframe(output);
+    }
 }
--- AozoraEditor/AozoraLibraryWasm/main.js
+++ AozoraEditor/AozoraLibraryWasm/main.js
@@ -9,17 +9,31 @@ const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
     .create();
 
 setModuleImports('main.js', {
-    window: {
-        location: {
-            href: () => globalThis.window.location.href
-        }
-    }
+    //window: {
+    //    location: {
+    //        href: () => globalThis.window.location.href
+    //    }
+    //}
+    WriteIframe: (html) => {
+        document.getElementById('outframe').srcdoc = html;
+    },
+
+    GetInputText: () => document.getElementById('input').value,
 });
 
 const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
-const text = exports.MyClass.Greeting();
-console.log(text);
+//const text = exports.MyClass.Greeting();
+//console.log(text);
 
-document.getElementById('out').innerHTML = text;
+document.getElementById('convertButton').onclick = () => {
+    exports.AozoraFunctions.Convert();
+}
+//document.getElementById('out').innerHTML = text;
 await dotnet.run();
+
+//function convert() {
+//    let input = document.getElementById('input').value;
+//    let html = exports.AozoraFunctions.Aozora2html(input);
+//    document.getElementById('outframe').src = "data:text/html;charset=utf-8," + escape(html);
+//}
