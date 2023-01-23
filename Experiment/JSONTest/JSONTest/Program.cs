d42448c366425b3eca1773faf9a999a0201c0c6b
kurema <kurema_makoto_software@yahoo.co.jp>
2023-01-23 20:47:11 +0900

    Moved Experimental Project / Add new test project

---
+++ Experiment/JSONTest/JSONTest/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sr = new StreamReader("snippets2.json");
+var snip = AozoraEditor.Snippets.FromJson(sr.ReadToEnd());
+
+var templateDic = new Dictionary<string, AozoraEditor.Template>();
+foreach(var item in snip.Templates)
+{
+	templateDic.Add(item.Id, item);
+}
+;
