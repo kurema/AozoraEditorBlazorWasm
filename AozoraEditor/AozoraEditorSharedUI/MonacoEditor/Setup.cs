7e48aa90d36e0a4c3dbc214e09215048b0b1a44c
kurema <kurema_makoto_software@yahoo.co.jp>
2023-02-10 14:11:33 +0900

    LearningBlazor

---
+++ AozoraEditor/AozoraEditorSharedUI/MonacoEditor/Setup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BlazorMonaco;
+using BlazorMonaco.Editor;
+
+namespace AozoraEditor.Shared.MonacoEditor;
+
+internal static partial class Setup
+{
+	public async static Task InitTheme()
+	{
+		await Global.DefineTheme("aozora-theme", new StandaloneThemeData()
+		{
+			Base = "vs",
+			Inherit = false,
+			Rules = new()
+			{
+				new TokenThemeRule{Token="ruby",Foreground="808080"},
+				new TokenThemeRule{Token="command",Foreground="00A000",FontStyle="bold"},
+				new TokenThemeRule{Token="accent",Foreground="0000A0"},
+			},
+			Colors = new Dictionary<string, string>()
+			{
+				["editor.foreground"] = "#000000",
+				//["editor.background"] = "#FFFFFF",
+			}
+		});
+
+		await Global.DefineTheme("aozora-theme-dark", new StandaloneThemeData()
+		{
+			Base = "vs-dark",
+			Inherit = true,
+			Rules = new()
+			{
+				new TokenThemeRule{Token="ruby",Foreground="808080"},
+				new TokenThemeRule{Token="command",Foreground="006000",FontStyle="bold"},
+				new TokenThemeRule{Token="accent",Foreground="000060"},
+			},
+			Colors = new Dictionary<string, string>()
+			{
+				["editor.foreground"] = "#FFFFFF",
+			}
+		});
+
+		await Global.SetTheme("aozora-theme");
+	}
+}
---
+++ AozoraEditor/AozoraEditorSharedUI/wwwroot/jsInterop.js
@@ -0,0 +1,39 @@
+﻿window.blazorMonaco.kurema = {
+    registerAozora: function () {
+        monaco.languages.register({ id: 'aozora' });
+        const keywords = [];//キーワードは基本［＃.+?］の中なので、別にハイライトをする必要はないと判断
+        monaco.languages.setMonarchTokensProvider('aozora', {
+            defaultToken: 'invalid',
+            keywords,
+            tokenizer: {
+                root: [
+                    [/《.*?》/, 'ruby'],
+                    [/※?［＃.+?］/, 'command'],
+                    [/〔.+?〕/, 'accent'],
+                ],
+            }
+        });
+
+        monaco.languages.registerCompletionItemProvider('aozora', {
+            provideCompletionItems: (model, position, context, token) => {
+                //サジェスト機能は行頭かスペースを入力後しか機能しない。従ってスペースを削除する。半角スペース以外なら削除不要。
+                let snipetDeleteSpace = [];
+                if (position.column > 2) { // columnはなぜか1から始まる。また文字入力後なので、2なら行頭。
+                    let rangeEdit = { startLineNumber: position.lineNumber, endLineNumber: position.lineNumber, startColumn: position.column - 2, endColumn: position.column - 1 };
+                    let charEdit = model.getValueInRange(rangeEdit);
+                    if (charEdit == ' ') { snipetDeleteSpace = [{ text: '', range: rangeEdit }]; }
+                }
+                var suggestions = [
+                    {
+                        label: 'ruby',
+                        kind: monaco.languages.CompletionItemKind.Snippet,
+                        insertText: '《${1:ルビ}》',
+                        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                        additionalTextEdits: snipetDeleteSpace,
+                    },
+                ];
+                return { suggestions: suggestions };
+            }
+        });
+    }
+}
