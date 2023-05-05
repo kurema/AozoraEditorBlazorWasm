75ca28fba6938948a9cbc0be9721dbc9e3584e92
kurema <kurema_makoto_software@yahoo.co.jp>
2023-05-05 13:30:02 +0900

    Update Tab

---
+++ AozoraEditor/AozoraEditorSharedUI/Shared/ITabPage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AozoraEditor.Shared.Shared;
+
+public interface ITabPage
+{
+	RenderFragment Title { get; }
+	event EventHandler? TitleChanged;
+}
