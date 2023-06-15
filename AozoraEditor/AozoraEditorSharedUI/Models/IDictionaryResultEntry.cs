413bd849784ac07e33e565e8a3cbb8d1f3d25651
kurema <kurema_makoto_software@yahoo.co.jp>
2023-06-16 00:04:56 +0900

    Update DictionaryPage

--- AozoraEditor/AozoraEditorSharedUI/Models/IDictionaryResultEntry.cs
+++ AozoraEditor/AozoraEditorSharedUI/Models/IDictionaryResultEntry.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AozoraEditor.Shared.Models;
@@ -12,18 +14,60 @@ public interface IDictionaryResultEntry
 {
 	IEnumerable<string> Characters { get; }
 	(int men, int ku, int ten) Jisx0213Code { get; }
+	IEnumerable<string> Unicode { get; }
+	string? Note { get; }
+	IEnumerable<UnicodeCategory?> UnicodeCategory { get; }
+	Guid Guid { get; }
 }
 
 public class DictionaryResultEntryGaijiChuki : IDictionaryResultEntry
 {
 	public entry Content { get; init; }
 
+	IEnumerable<UnicodeCategory?>? _UnicodeCategory;
+	public IEnumerable<UnicodeCategory?> UnicodeCategory
+	{
+		get
+		{
+			if (_UnicodeCategory is not null)
+			{
+				foreach (var item in _UnicodeCategory) yield return item;
+				yield break;
+			}
+			var result = new List<UnicodeCategory?>();
+			foreach (var item in Content?.characters?.character ?? Array.Empty<string>())
+			{
+				UnicodeCategory? ch = item.Length == 0 ? null : CharUnicodeInfo.GetUnicodeCategory(item, 0);
+				result.Add(ch);
+				yield return ch;
+			}
+			_UnicodeCategory = result.ToArray();
+		}
+	}
+
 	public DictionaryResultEntryGaijiChuki(entry content)
 	{
 		Content = content ?? throw new ArgumentNullException(nameof(content));
+		Guid = Guid.NewGuid();
 	}
 
-	public IEnumerable<string> Characters => Content?.characters?.character ?? Array.Empty<string>();
+	public IEnumerable<string> Characters
+	{
+		get
+		{
+			//if (UnicodeCategory.FirstOrDefault() == System.Globalization.UnicodeCategory.PrivateUse && Content?.note?.Item is noteUnicode note)
+			if (Content?.note?.Item is noteUnicode note)
+			{
+				return new[] { char.ConvertFromUtf32(Convert.ToInt32(note.code, 16)).ToString() };
+			}
+			else if (Content?.note?.Item is noteJisx0213 jisx0213)
+			{
+				var crct = Aozora.Helpers.YamlValues.Jisx0213ToString(jisx0213.men, jisx0213.ku, jisx0213.ten);
+				if (crct is not null) return new[] { crct };
+			}
+			return Content?.characters?.character ?? Array.Empty<string>();
+		}
+	}
 
 	public (int men, int ku, int ten) Jisx0213Code
 	{
@@ -41,6 +85,30 @@ public class DictionaryResultEntryGaijiChuki : IDictionaryResultEntry
 	}
 
 	public static (int men, int ku, int ten) Jisx0213CodeEmpty => (-1, -1, -1);
+
+	public IEnumerable<string> Unicode
+	{
+		get
+		{
+			if (Content?.note?.Item is noteUnicode noteUnicode)
+			{
+				return new[] { $"U+{noteUnicode.code}" };
+			}
+			return GetUnicode(string.Join("", Characters));
+		}
+	}
+
+	public string? Note => Content?.note?.full;
+
+	public Guid Guid { get; }
+
+	public static IEnumerable<string> GetUnicode(string text)
+	{
+		foreach (var item in text.EnumerateRunes())
+		{
+			yield return $"U+{item.Value:X}";
+		}
+	}
 }
 
 public class DictionaryResultSingleChar : IDictionaryResultEntry
@@ -50,6 +118,25 @@ public class DictionaryResultSingleChar : IDictionaryResultEntry
 	public DictionaryResultSingleChar(string content)
 	{
 		Character = content ?? throw new ArgumentNullException(nameof(content));
+		Guid = Guid.NewGuid();
+	}
+
+	UnicodeCategory? _UnicodeCategory;
+	public IEnumerable<UnicodeCategory?> UnicodeCategory
+	{
+		get
+		{
+			return new UnicodeCategory?[] { _UnicodeCategory ??= CharUnicodeInfo.GetUnicodeCategory(Character, 0) };
+		}
+	}
+
+
+	public IEnumerable<string> Unicode
+	{
+		get
+		{
+			return DictionaryResultEntryGaijiChuki.GetUnicode(Character);
+		}
 	}
 
 	public IEnumerable<string> Characters
@@ -69,4 +156,15 @@ public class DictionaryResultSingleChar : IDictionaryResultEntry
 			return value;
 		}
 	}
+
+	public string? Note
+	{
+		get
+		{
+			return null;
+		}
+	}
+
+	public Guid Guid { get; }
+
 }
