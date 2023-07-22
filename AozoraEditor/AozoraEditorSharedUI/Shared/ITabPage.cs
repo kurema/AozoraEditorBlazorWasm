3d5b8b0df1e02a3094643198d6bdfee090eace3a
kurema <kurema_makoto_software@yahoo.co.jp>
2023-07-23 02:46:17 +0900

    RenderFragment is difficult!

--- AozoraEditor/AozoraEditorSharedUI/Shared/ITabPage.cs
+++ AozoraEditor/AozoraEditorSharedUI/Shared/ITabPage.cs
@@ -14,6 +14,9 @@ public interface ITabPage
 	event EventHandler? TitleChanged;
 
 	RenderFragment Body { get; }
+	Type DynamicType { get; }
+	IDictionary<string, object> DynamicParamaters { get; }
+
 	ObservableCollection<ICommandEntry> PanelSwitchCommands { get; }
 	ObservableCollection<ICommandEntry> MainCommands { get; }
 
