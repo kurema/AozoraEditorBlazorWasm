8edf1cf263f325914d49ecf7d79e73cf92f62866
kurema <kurema_makoto_software@yahoo.co.jp>
2023-08-01 22:57:10 +0900

    Moved namespace to EnvironmentAbstractionLayer

---
+++ AozoraEditor/AozoraEditor.Wasm2/Models/ApplicationModel.cs
@@ -0,0 +1,24 @@
+﻿using AozoraEditor.Shared.Models.EnvironmentAbstractionLayer;
+using Blazored.LocalStorage;
+
+namespace AozoraEditor.Wasm.Models;
+
+public class ApplicationModel : IApplicationModel, ILocalStorageString
+{
+	public ApplicationModel(ILocalStorageService localStorageService)
+	{
+		LocalStorageService = localStorageService ?? throw new ArgumentNullException(nameof(localStorageService));
+	}
+
+	public Blazored.LocalStorage.ILocalStorageService LocalStorageService { get; init; }
+
+	public async Task<string> LoadLocal(string tag)
+	{
+		return await LocalStorageService.GetItemAsStringAsync(tag);
+	}
+
+	public async Task SaveLocal(string tag, string text)
+	{
+		await LocalStorageService.SetItemAsStringAsync(tag, text);
+	}
+}
---
+++ AozoraEditor/AozoraEditorSharedUI/Models/EnvironmentAbstractionLayer/IApplicationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AozoraEditor.Shared.Models.EnvironmentAbstractionLayer;
+
+//EnvironmentAbstractionLayerなんて言わずに普通にDI使えよ感が出てきた。まぁいいや。割とDIアレルギーがある。
+
+public interface ILocalStorageString
+{
+    //MAUIやWasmのような実行環境毎に異なる挙動を吸収するインターフェース。
+    //Cascadeすれば良いだけなのでDIとか使う必要はない。
+
+    Task SaveLocal(string tag, string text);
+    Task<string> LoadLocal(string tag);
+}
+
+
+public interface IApplicationModel
+{
+
+}
