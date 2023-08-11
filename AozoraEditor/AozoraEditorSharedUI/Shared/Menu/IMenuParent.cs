7a9be129857af5a086e6aef6af63ba8c3230ad82
kurema <kurema_makoto_software@yahoo.co.jp>
2023-08-11 12:56:51 +0900

    Quick sync: IMenuItemProvider should use RenderFragment...

--- AozoraEditor/AozoraEditorSharedUI/Shared/Menu/IMenuParent.cs
+++ AozoraEditor/AozoraEditorSharedUI/Shared/Menu/IMenuParent.cs
@@ -29,7 +29,7 @@ public interface IMenuItem
 	IMenuParent? Parent { get; }
 }
 
-public interface IMenuItemProvider
+public interface IMenuItemProvider : IMenuItem
 {
 	//Hide()を実装していないので、現状子持ちのIMenuItemSingleは正しい挙動にならない点に注意。
 	IEnumerable<IMenuItemSingle> Items { get; }
